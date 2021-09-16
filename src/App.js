6db3bc40a515d00df80bbcd8974ae9c9165b1ac9
Gaston <gaston@pitagorasc.com.ar>
2021-09-16 08:00:37 -0300

    search todos

--- src/App.js
+++ src/App.js
@@ -21,6 +21,16 @@ function App() {
   const [ searchValue, setSearchValue ] = React.useState('');
   const completedTodos = todos.filter(todo => !!todo.completed).length;
   const totalTodos = todos.length;
+  let searchedTodos = [];
+  if (!searchValue.length >= 1) {
+    searchedTodos = todos;
+  } else {
+    searchedTodos = todos.filter(todo => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText);
+    });
+  }
   return (
     <React.Fragment>
       <TodoTitle />
@@ -29,12 +39,13 @@ function App() {
         completed={completedTodos}
 
       />
+      {/* TODO: Agregar un componente para que desaparezcan los ya completados */}
       <TodoSearch 
         searchValue={searchValue}
         setSearchValue={setSearchValue}
       />
       <TodoList>
-        {todos.map(todo => (
+        {searchedTodos.map(todo => (
           <TodoItem 
           key={todo.text} 
           text={todo.text} 
