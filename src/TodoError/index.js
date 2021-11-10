5bf23de18d850d1b3854a18e5c530ee1a2243f06
Gaston <gaston@pitagorasc.com.ar>
2021-11-10 12:14:48 -0300

    error component

--- src/TodoError/index.js
+++ src/TodoError/index.js
@@ -1,7 +1,10 @@
 import React from "react";
+import './TodoError.css';
 
 function TodoError({ error }) {
-    return <p>Ohh por Dios!! que paso??</p>
+    return <div className='img-error-todo'>
+                <img src="https://i.ibb.co/hBY1bhv/error-404.png" alt="error to do app"/>
+            </div>
 }
 
 export { TodoError };
